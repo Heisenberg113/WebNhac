ede44af24f6f56c0c9e53ef8811ced29c4c3423f
Heisenberg113 <hellspawnscorpion1@gmail.com>
2024-11-14 02:43:12 +0700

    1

---
+++ Test1/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Remoting.Messaging;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Test1.Controllers.Admin
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Layout = "~/Views/Shared/_AdminLayout.cshtml";
+            return View();
+        }
+        public ActionResult Singers()
+        {
+            ViewBag.Layout = "~/Views/Shared/_AdminLayout.cshtml";
+            return View();
+        }
+        public ActionResult Types()
+        {
+            ViewBag.Layout = "~/Views/Shared/_AdminLayout.cshtml";
+            return View();
+        }
+        public ActionResult Songs()
+        {
+            ViewBag.Layout = "~/Views/Shared/_AdminLayout.cshtml";
+            return View();
+        }
+        public ActionResult Users()
+        {
+            ViewBag.Layout = "~/Views/Shared/_AdminLayout.cshtml";
+            return View();
+        }
+    }
+}
