73a225a4b520bfabc3096e2d2b70fba0c49c900c
Heisenberg113 <hellspawnscorpion1@gmail.com>
2024-11-18 03:54:40 +0700

    Intitalize

--- Test1/Controllers/AdminController.cs
+++ Test1/Controllers/AdminController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Runtime.Remoting.Messaging;
 using System.Web.Http;
 using System.Web.Mvc;
+using Test1.Models;
 
 namespace Test1.Controllers.Admin
 {
@@ -19,22 +20,34 @@ namespace Test1.Controllers.Admin
         public ActionResult Singers()
         {
             ViewBag.Layout = "~/Views/Shared/_AdminLayout.cshtml";
-            return View();
+            WebNgheNhacEntities db = new WebNgheNhacEntities();
+            List<Singers> t = db.Singers.ToList();
+            ViewBag.Allsinger = t;
+            return PartialView("Singers");
         }
         public ActionResult Types()
         {
             ViewBag.Layout = "~/Views/Shared/_AdminLayout.cshtml";
-            return View();
+            WebNgheNhacEntities db = new WebNgheNhacEntities();
+            List<Types> t = db.Types.ToList();
+            ViewBag.Alltype = t;
+            return PartialView("Types");
         }
         public ActionResult Songs()
         {
             ViewBag.Layout = "~/Views/Shared/_AdminLayout.cshtml";
-            return View();
+            WebNgheNhacEntities db = new WebNgheNhacEntities();
+            List<Songs> t = db.Songs.ToList();
+            ViewBag.Allsong = t;
+            return PartialView("Songs");
         }
         public ActionResult Users()
         {
             ViewBag.Layout = "~/Views/Shared/_AdminLayout.cshtml";
-            return View();
+            WebNgheNhacEntities db = new WebNgheNhacEntities();
+            List<Users> t = db.Users.ToList();
+            ViewBag.Alluser = t;
+            return PartialView("Users");
         }
     }
 }
